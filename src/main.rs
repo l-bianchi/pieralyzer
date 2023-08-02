858571a87c24fa3eeefc1cdaf2a0717823e53b4f
l-bianchi <illorenzobianchi@proton.me>
2023-08-02 16:43:45 +0200

    fix issue with parameter and add vector support

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use clap::Parser;
 
 #[derive(Parser)]
+#[command(author, version, about, long_about = None)]
 struct Cli {
-    #[arg(short, long)]
-    param: String,
+    text: Vec<String>,
 }
 
 fn main() {
     let args = Cli::parse();
+    let mut result = "".to_owned();
 
-    println!("pier-{}", args.param);
+    for (idx, text) in args.text.iter().enumerate() {
+        let pier = "pier-";
+
+        if idx > 0 {
+            result.push_str(" ")
+        }
+
+        result.push_str(pier);
+        result.push_str(&text);
+    }
+
+    print!("{}", result)
 }
